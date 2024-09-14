ae6b36bdb63f28fc305c561456eb50b533551b91
Nelson-5553 <andresnelson2004@hotmail.com>
2024-09-13 20:26:53 -0500

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless'; // Importa el adaptador de Vercel
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: 'server', // Cambia la salida a 'server' para que use serverless en Vercel
+  adapter: vercel(), // Configura el adaptador de Vercel
 });
