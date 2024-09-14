47c0c464d9671f318d02d4b30ae543ad0d000f87
Nelson-5553 <andresnelson2004@hotmail.com>
2024-09-13 21:08:38 -0500

    serverlees

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/static'; // Importa el adaptador de Vercel
+import vercel from '@astrojs/vercel/serverless'; // Importa el adaptador de Vercel
 
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'static', // Cambia la salida a 'server' para que use serverless en Vercel
-  adapter: vercel(), // Configura el adaptador de Vercel
+  output: 'server', // Cambia la salida a 'server' para que use serverless en Vercel
+  adapter: vercel({ 
+    edgeMiddleware: true,
+  }), // Configura el adaptador de Vercel
 });
