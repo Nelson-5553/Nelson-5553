5e4b7855b6ce3fbdddef8110de4c07aeaa02a47d
Nelson-5553 <andresnelson2004@hotmail.com>
2024-09-13 20:46:28 -0500

    vercel static 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import vercel from '@astrojs/vercel/static'; // Importa el adaptador de Vercel
 
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server', // Cambia la salida a 'server' para que use serverless en Vercel
+  output: 'static', // Cambia la salida a 'server' para que use serverless en Vercel
   adapter: vercel(), // Configura el adaptador de Vercel
 });
