f775d6bfe6ed69a8649673c6a2e7d1bbe8150bd6
Nelson-5553 <andresnelson2004@hotmail.com>
2024-09-18 09:18:41 -0500

    Enrutamiento entre pagina

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless'; // Importa el adaptador de Vercel
 
 export default defineConfig({
+  prefetch:true,
   integrations: [tailwind()],
   output: 'server', // Cambia la salida a 'server' para que use serverless en Vercel
   adapter: vercel({ 
