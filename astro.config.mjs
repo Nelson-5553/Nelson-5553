8ae3615c3d9dab67ab76ad953878ca264dc688c5
Nelson-5553 <andresnelson2004@hotmail.com>
2024-12-12 00:44:12 -0500

    version de astro actualizada y swappy funcionando

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless'; // Importa el adaptador de Vercel
 
+import react from '@astrojs/react';
+
 export default defineConfig({
   prefetch:true,
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
   output: 'server', // Cambia la salida a 'server' para que use serverless en Vercel
   adapter: vercel({ 
     edgeMiddleware: true,
